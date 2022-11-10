adee53ce9d218c71f684f3c4941ecd8e44ad5e93
mebysshe <117329309+mebysshe@users.noreply.github.com>
2022-11-09 20:21:00 -0800

    add project-3

---
+++ project-3/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#flower').click(function () {
+            $('.pop-up').toggle();
+        });
+    $('#imgage1').click(function () {
+        $('.infopanel').toggle(2000);     
+        $(this).toggleClass("bigger");
+        });
+});
